b8e06b8174a051e8084f9e6af25a20549faa2edc
Sajithsaji007 <saji0151@gmail.com>
2022-08-05 01:29:31 +0530

    First commit

---
+++ MavenDemoArtifact/src/main/java/MavenDemoProj/MavenDemoArtifact/App.java
@@ -0,0 +1,13 @@
+package MavenDemoProj.MavenDemoArtifact;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
